5f43984ce1cee7b0b8c438899729c5edade45168
Ryan Vail <ryanvail64@gmail.com>
2025-09-05 20:23:51 -0400

    glsl.parser: Added precedence fns to operator.zig

--- src/glsl/parser/operator.zig
+++ src/glsl/parser/operator.zig
@@ -3,6 +3,26 @@ const std = @import("std");
 const parser = @import("../parser.zig");
 const Token = parser.Token;
 
+pub const Weight = enum {
+    const Self = @This();
+
+    @"0",
+    @"1",
+    @"2",
+    @"3",
+    @"4",
+    @"5",
+    @"6",
+    @"7",
+    @"8",
+    @"9",
+    @"10",
+    @"11",
+    @"12",
+
+    pub const max = Self.@"12";
+};
+
 pub const Op = enum {
     const Self = @This();
 
@@ -118,6 +138,81 @@ pub const Op = enum {
             .bxor_assign => "^=",
             .bor_assign => "|=",
             .band_assign => "&=",
+        };
+    }
+
+    pub fn appliesToLeft(self: Self) bool {
+        return switch (self) {
+            .inc, .dec => true,
+            else => false,
+        };
+    }
+
+    pub fn appliesToRight(self: Self) bool {
+        return switch (self) {
+            .sub, .bnot, .lnot => true,
+            else => false,
+        };
+    }
+
+    pub fn appliesToDual(self: Self) bool {
+        return switch (self) {
+            .add,
+            .sub,
+            .mul,
+            .div,
+            .mod,
+            .bxor,
+            .bor,
+            .band,
+            .land,
+            .lxor,
+            .lor,
+            .eq,
+            .ne,
+            .lt,
+            .gt,
+            .le,
+            .ge,
+            .field,
+            .assign,
+            .add_assign,
+            .sub_assign,
+            .mul_assign,
+            .div_assign,
+            .mod_assign,
+            .bxor_assign,
+            .bor_assign,
+            .band_assign,
+            => true,
+            else => false,
+        };
+    }
+
+    pub fn getWeight(self: Self) Weight {
+        return switch (self) {
+            .field, .inc, .dec => .@"0",
+            .bnot, .lnot => .@"1",
+            .mul, .div, .mod => .@"2",
+            .add, .sub => .@"3",
+            .lt, .gt, .le, .ge => .@"4",
+            .eq, .ne => .@"5",
+            .band => .@"6",
+            .bxor => .@"7",
+            .bor => .@"8",
+            .land => .@"9",
+            .lxor => .@"10",
+            .lor => .@"11",
+            .assign,
+            .add_assign,
+            .sub_assign,
+            .mul_assign,
+            .div_assign,
+            .mod_assign,
+            .bxor_assign,
+            .bor_assign,
+            .band_assign,
+            => .@"12",
         };
     }
 };
